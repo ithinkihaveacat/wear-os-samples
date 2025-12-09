4a525b61051bbdb07e542b5363eb71ef413cd55e
Michael Stillwell <mjs@beebo.org>
2025-12-09 22:59:45 +0000

    Spotless updates

--- WearTilesKotlin/app/src/main/java/com/example/wear/tiles/glance/HelloWidget.kt
+++ WearTilesKotlin/app/src/main/java/com/example/wear/tiles/glance/HelloWidget.kt
@@ -26,34 +26,34 @@ import androidx.compose.remote.creation.compose.modifier.RemoteModifier
 import androidx.compose.remote.creation.compose.modifier.background
 import androidx.compose.remote.creation.compose.modifier.fillMaxSize
 import androidx.compose.remote.creation.compose.state.RemoteColor
-import androidx.wear.compose.remote.material3.RemoteMaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.glance.wear.GlanceWearWidget
 import androidx.glance.wear.GlanceWearWidgetService
 import androidx.glance.wear.WearWidgetDocument
 import androidx.glance.wear.WearWidgetParams
+import androidx.wear.compose.remote.material3.RemoteMaterialTheme
 
 class HelloWidget : GlanceWearWidget() {
-  override suspend fun provideWidgetData(context: Context, params: WearWidgetParams) =
-    WearWidgetDocument { HelloWidgetContent() }
+    override suspend fun provideWidgetData(context: Context, params: WearWidgetParams) =
+        WearWidgetDocument { HelloWidgetContent() }
 }
 
 @SuppressLint("RestrictedApi")
 @RemoteComposable
 @Composable
 fun HelloWidgetContent() {
-  RemoteMaterialTheme {
-    RemoteBox(
-      modifier = RemoteModifier.fillMaxSize().background(RemoteColor(Color.Red)),
-      horizontalAlignment = RemoteAlignment.CenterHorizontally,
-      verticalArrangement = RemoteArrangement.Center,
-    ) {
-      RemoteText(text = "Hello RemoteCompose!", color = RemoteColor(Color.White))
+    RemoteMaterialTheme {
+        RemoteBox(
+            modifier = RemoteModifier.fillMaxSize().background(RemoteColor(Color.Red)),
+            horizontalAlignment = RemoteAlignment.CenterHorizontally,
+            verticalArrangement = RemoteArrangement.Center
+        ) {
+            RemoteText(text = "Hello RemoteCompose!", color = RemoteColor(Color.White))
+        }
     }
-  }
 }
 
 class HelloWidgetService() : GlanceWearWidgetService() {
-  override val widget: GlanceWearWidget = HelloWidget()
+    override val widget: GlanceWearWidget = HelloWidget()
 }
