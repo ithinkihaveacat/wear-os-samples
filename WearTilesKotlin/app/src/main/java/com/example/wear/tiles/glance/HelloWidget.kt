1c81f8c22f6809eac36a5af9e91e906aa0d9dc87
Michael Stillwell <mjs@beebo.org>
2025-12-12 18:16:05 +0000

    Bump kotlin version

--- WearTilesKotlin/app/src/main/java/com/example/wear/tiles/glance/HelloWidget.kt
+++ WearTilesKotlin/app/src/main/java/com/example/wear/tiles/glance/HelloWidget.kt
@@ -1,3 +1,5 @@
+@file:Suppress("RestrictedApi")
+
 /*
  * Copyright 2025 The Android Open Source Project
  *
@@ -5,7 +7,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     https://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,45 +17,49 @@
  */
 package com.example.wear.tiles.glance
 
-import android.annotation.SuppressLint
 import android.content.Context
+import androidx.compose.remote.creation.compose.capture.painter.painterRemoteColor
 import androidx.compose.remote.creation.compose.layout.RemoteAlignment
 import androidx.compose.remote.creation.compose.layout.RemoteArrangement
 import androidx.compose.remote.creation.compose.layout.RemoteBox
 import androidx.compose.remote.creation.compose.layout.RemoteComposable
 import androidx.compose.remote.creation.compose.layout.RemoteText
 import androidx.compose.remote.creation.compose.modifier.RemoteModifier
-import androidx.compose.remote.creation.compose.modifier.background
 import androidx.compose.remote.creation.compose.modifier.fillMaxSize
 import androidx.compose.remote.creation.compose.state.RemoteColor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.glance.wear.GlanceWearWidget
 import androidx.glance.wear.GlanceWearWidgetService
+import androidx.glance.wear.WearWidgetData
 import androidx.glance.wear.WearWidgetDocument
 import androidx.glance.wear.WearWidgetParams
-import androidx.wear.compose.remote.material3.RemoteMaterialTheme
+import com.example.wear.tiles.R
+
+class HelloWidgetService : GlanceWearWidgetService() {
+    override val widget: GlanceWearWidget = HelloWidget()
+}
 
 class HelloWidget : GlanceWearWidget() {
-    override suspend fun provideWidgetData(context: Context, params: WearWidgetParams) =
-        WearWidgetDocument { HelloWidgetContent() }
+    override suspend fun provideWidgetData(
+        context: Context,
+        params: WearWidgetParams,
+    ): WearWidgetData =
+        WearWidgetDocument(backgroundPainter = painterRemoteColor(Color.Red)) {
+            HelloWidgetContent()
+        }
 }
 
-@SuppressLint("RestrictedApi")
 @RemoteComposable
 @Composable
+@Suppress("RestrictedApiAndroidX")
 fun HelloWidgetContent() {
-    RemoteMaterialTheme {
-        RemoteBox(
-            modifier = RemoteModifier.fillMaxSize().background(RemoteColor(Color.Red)),
-            horizontalAlignment = RemoteAlignment.CenterHorizontally,
-            verticalArrangement = RemoteArrangement.Center
-        ) {
-            RemoteText(text = "Hello RemoteCompose!", color = RemoteColor(Color.White))
-        }
+    RemoteBox(
+        modifier = RemoteModifier.fillMaxSize(),
+        horizontalAlignment = RemoteAlignment.CenterHorizontally,
+        verticalArrangement = RemoteArrangement.Center,
+    ) {
+        RemoteText(text = stringResource(R.string.hello), color = RemoteColor(Color.Black))
     }
-}
-
-class HelloWidgetService() : GlanceWearWidgetService() {
-    override val widget: GlanceWearWidget = HelloWidget()
 }
