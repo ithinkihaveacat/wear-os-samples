1f7c4a14bc319854bd3972097fe51b8ee4b60f93
Michael Stillwell <mjs@beebo.org>
2025-12-16 17:29:44 +0000

    Updates

--- WearWidgetKotlin/app/src/main/java/com/google/example/wear_widget/WidgetCatalog.kt
+++ WearWidgetKotlin/app/src/main/java/com/google/example/wear_widget/WidgetCatalog.kt
@@ -4,7 +4,6 @@ package com.google.example.wear_widget
 
 import android.annotation.SuppressLint
 import android.content.Context
-import androidx.compose.remote.creation.ExperimentalRemoteCreationApi
 import androidx.compose.remote.creation.compose.capture.painter.painterRemoteColor
 import androidx.compose.remote.creation.compose.layout.RemoteAlignment
 import androidx.compose.remote.creation.compose.layout.RemoteArrangement
@@ -14,8 +13,6 @@ import androidx.compose.remote.creation.compose.layout.RemoteText
 import androidx.compose.remote.creation.compose.modifier.RemoteModifier
 import androidx.compose.remote.creation.compose.modifier.fillMaxSize
 import androidx.compose.remote.creation.compose.state.RemoteColor
-import androidx.compose.remote.creation.profile.RcPlatformProfiles
-import androidx.compose.remote.tooling.preview.RemotePreview
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
@@ -24,7 +21,6 @@ import androidx.glance.wear.GlanceWearWidgetService
 import androidx.glance.wear.WearWidgetData
 import androidx.glance.wear.WearWidgetDocument
 import androidx.glance.wear.WearWidgetParams
-import androidx.wear.compose.ui.tooling.preview.WearPreviewDevices
 
 class WidgetCatalogService : GlanceWearWidgetService() {
     override val widget: GlanceWearWidget = WidgetCatalog()
@@ -36,13 +32,13 @@ class WidgetCatalog : GlanceWearWidget() {
         params: WearWidgetParams,
     ): WearWidgetData =
         WearWidgetDocument(backgroundPainter = painterRemoteColor(Color.Blue)) {
-            WidgetCatalogContent()
+            WidgetContent()
         }
 }
 
 @RemoteComposable
 @Composable
-fun WidgetCatalogContent() {
+fun WidgetContent() {
     RemoteBox(
         modifier = RemoteModifier.fillMaxSize(),
         horizontalAlignment = RemoteAlignment.CenterHorizontally,
@@ -50,12 +46,4 @@ fun WidgetCatalogContent() {
     ) {
         RemoteText(text = stringResource(R.string.hello), color = RemoteColor(Color.White))
     }
-}
-
-@OptIn(ExperimentalRemoteCreationApi::class)
-@WearPreviewDevices
-@Composable
-fun WidgetCatalogPreview() {
-    val content: @Composable @RemoteComposable () -> Unit = { WidgetCatalogContent() }
-    RemotePreview(RcPlatformProfiles.WEAR_WIDGETS, content)
 }
